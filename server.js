051ee7dd8f2ec467a1e0347ca8fb55454ac3550c
Kartikey Goel <kartikeygoel093@gmail.com>
2021-08-18 02:11:53 +0530

    ready for deployment

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use("/api/bookings", bookingsRoute);
 
 if (process.env.NODE_ENV == "PROD") {
   app.use(express.static(path.resolve(__dirname, "client", "build")));
-  app.get("/*", (req, res) => {
+  app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
 }
